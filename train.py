5dedd05d06cacd1cf026fa14adc36de3b6d62025
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-12-18 11:28:14 +0530

    Delete train.py

--- train.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-def train_model(model, X_train, y_train):
-    datagen = ImageDataGenerator(validation_split=0.2)
-    train_gen = datagen.flow(X_train, y_train, batch_size=32, subset='training')
-    val_gen = datagen.flow(X_train, y_train, batch_size=32, subset='validation')
-    history = model.fit(train_gen, validation_data=val_gen, epochs=10)
-    return history
