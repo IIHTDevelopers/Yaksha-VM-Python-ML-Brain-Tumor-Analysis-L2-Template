a8ef8c181cfb3ecb5d3bb545504ad55a5c50fe0c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-12-18 11:27:17 +0530

    Delete test directory

--- test/test_exceptional.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from test.TestUtils import TestUtils
-class ExceptionalTest(unittest.TestCase):
-    def test_exception(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestException",True,"exception")
-        print("TestException = Passed")
